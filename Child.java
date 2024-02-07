e7c00dec370fff05683c02e0c089a935d1bfb128
01Fe22bca139 <01fe22bca139@kletech.ac.in>
2024-02-07 15:54:21 +0530

    Update a program

---
+++ Child.java
@@ -0,0 +1,13 @@
+public class Child{
+    public  float square(float operand_1){
+          return(operand_1*operand_1);
+      }
+  
+      public float cube(float operand_1){
+          return(operand_1*operand_1*operand_1);
+      }
+  
+     public float mod(float operand_1,float operand_2){
+          return(operand_1%operand_2);
+      }
+  }
